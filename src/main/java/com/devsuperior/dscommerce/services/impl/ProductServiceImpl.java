c02cdb372a5954884abba0c4c58d819197796e11
= <=>
2023-09-03 00:09:02 -0300

    Search with name

--- src/main/java/com/devsuperior/dscommerce/services/impl/ProductServiceImpl.java
+++ src/main/java/com/devsuperior/dscommerce/services/impl/ProductServiceImpl.java
@@ -31,8 +31,8 @@ public class ProductServiceImpl implements ProductService {
     }
     @Transactional(readOnly = true)
     @Override
-    public Page<ProductDTO> findAll(Pageable pageable) {
-        Page<Product> result = repository.findAll(pageable);
+    public Page<ProductDTO> findAll(String name, Pageable pageable) {
+        Page<Product> result = repository.searchByName(name, pageable);
         return result.map(x -> mapToDTO(x));
     }
     @Transactional
